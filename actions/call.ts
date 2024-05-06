154b87a65b13086ef10d2ad818f466e6f01b0697
cywinito <alexcywiner@hotmail.com>
2024-05-06 00:32:45 -0300

    Fetch data for the upcoming call banner

---
+++ actions/call.ts
@@ -0,0 +1,31 @@
+"use server";
+
+import { currentUser } from "@clerk/nextjs/server";
+
+import { getClient } from "./client";
+
+export const getFirstUpcomingCall = async () => {
+  const user = await currentUser();
+  if (!user) throw new Error("User is not logged in");
+
+  const client = getClient();
+
+  const { calls } = await client.video.queryCalls({
+    sort: [{ field: "starts_at", direction: -1 }],
+    filter_conditions: {
+      starts_at: {
+        $exists: true,
+      },
+      $or: [
+        {
+          created_by_user_id: user.id,
+        },
+        {
+          members: { $in: [user.id] },
+        },
+      ],
+    },
+  });
+  const upcomingCallStr = calls[0]?.call?.starts_at;
+  return upcomingCallStr ? new Date(upcomingCallStr) : null;
+};
---
+++ actions/client.ts
@@ -0,0 +1,13 @@
+import { StreamClient } from "@stream-io/node-sdk";
+
+const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+const apiSecret = process.env.STREAM_SECRET_KEY;
+
+export const getClient = () => {
+  if (!apiKey) throw new Error("Missing Stream API key");
+  if (!apiSecret) throw new Error("Missing Stream API secret");
+
+  const client = new StreamClient(apiKey, apiSecret);
+
+  return client;
+};
